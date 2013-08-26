f91d603dc5e355db3013a7224132e2da1b8ee937
J C Lawrence <claw@kanga.nu>
2013-08-25 18:04:56 -0700

    Tagging

--- xxpaper/__init__.py
+++ xxpaper/__init__.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
 
-import xxpaper.metaversion as metaversion
+#import xxpaper.metaversion as metaversion
 import xxpaper.version as version
+from xxpaper.version import __version__, __version_info__
 from xxpaper.sheet import Sheet
 from xxpaper.charter import Charter
 from xxpaper.private import Private
