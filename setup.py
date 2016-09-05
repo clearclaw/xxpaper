f97b312a934ce59589f4c84a02f81ca1a069f57b
J C Lawrence <claw@kanga.nu>
2016-09-05 11:28:38 -0700

    GPL v3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup (
     author = "J C Lawrence",
     author_email = "claw@kanga.nu",
     url = "https://github.com/clearclaw/xxpaper",
-    license = "Creative Commons Attribution-ShareAlike 3.0 Unported",
+    license = "GPL v3.0",
     packages = find_packages (exclude = ["tests",]),
     include_package_data = True,
     zip_safe = False,
